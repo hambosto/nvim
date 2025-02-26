2eb4bbe2ab0f3e74b9259b2e4e1caf2494ea8cf7
Ilham Putra Husada <hambosto@gmail.com>
2025-02-26 23:21:21 +0700

    chore(deps): update plugins in lazy-lock.json

--- lua/community.lua
+++ lua/community.lua
@@ -7,7 +7,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
+  -- { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.go" },
   -- { import = "astrocommunity.pack.typescript" },
   -- { import = "astrocommunity.pack.python" },
